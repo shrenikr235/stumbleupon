d1061b2b7a06f7132081049a0d9e57156015f729
Shrenik Raj <shrenikr235@gmail.com>
2022-05-20 10:43:50 +0530

    Secret key is now hidden

--- stumbleupon/settings.py
+++ stumbleupon/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0cb3nq%-oo=8i&vcej9q6n=@uqcc1e^zq-@b&n7j)-u0!&^8wh'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
