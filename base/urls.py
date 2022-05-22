73f9f2cdb2eb46c04295bf9afee6cadc81873054
Shrenik Raj <shrenikr235@gmail.com>
2022-05-22 20:55:06 +0530

    user login and flash messages in progress

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    path("login/", views.login_page,name="login"),
     path("", views.home, name="home"),
     path("room/<str:pk>", views.room, name="room"), 
     path("create-room/", views.create_room, name="create-room"),
--- base/views.py
+++ base/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # rooms = [
 #     {"id":1, "name":"Learn JavaScript"},
@@ -16,6 +19,27 @@ from django.db.models import Q
 queryset = ModelName.objects.all()
 
 """
+
+def login_page(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
+        else:
+            messages.error(request, "Username or password does not exist")
+
+    context = {}
+    return render(request, "base/login_register.html", context)
 
 def home(request):
     # return HttpResponse("Home Page") # without templates
