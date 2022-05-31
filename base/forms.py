7e77a55b902512a3aa76e2cf69340f02ce2244ce
Shrenik Raj <shrenikr235@gmail.com>
2022-05-31 20:51:11 +0530

    changes made to user a/c page

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
@@ -9,4 +10,8 @@ class RoomForm(ModelForm):
         # create fields based on the fields (host, topic, name, desc etc) in the Room model.
         # later on, fields = ["name", "body"] or only the required fields
         exclude = ["host", "participants"]
-        
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "email"]
--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from urllib.request import Request
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib import messages
@@ -205,3 +205,18 @@ def delete_message(request, pk):
         message.delete()
         return redirect("home")
     return render(request, "base/delete.html", {"obj": message})
+
+
+@login_required(login_url="login")
+def update_user(request):
+    user = request.user
+    form = UserForm(instance = user)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance = user)
+        if form.is_valid():
+            form.save()
+            return redirect("user-profile", pk = user.id)
+
+
+    return render(request, "base/update-user.html", {"form": form})
