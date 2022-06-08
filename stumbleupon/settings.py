cd4adddb6d440b8e8cdead4d20d57ff91af60570
Shrenik Raj <shrenikr235>
2022-06-08 14:47:41 +0000

    deployed to pythonanywhere

--- stumbleupon/settings.py
+++ stumbleupon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0cb3nq%-oo=8i&vcej9q6n=@uqcc1e^zq-@b&n7j)-u0!&^8wh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shrenikr235.pythonanywhere.com']
 
 
 # Application definition
@@ -125,11 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/shrenikr235/stumbleupon/static'
 MEDIA_URL = '/images/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static"
+# ]
 
 # for user-uploaded content (avatar)
 MEDIA_ROOT = BASE_DIR / "static/images"
